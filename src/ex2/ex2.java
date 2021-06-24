58f4098047ba944c43ce58243d0400f155add1d4
any-gomes <52418846+any-gomes@users.noreply.github.com>
2021-06-23 22:38:55 -0300

    Delete src directory

--- src/ex2/ex2.java
+++
@@ -1,9 +0,0 @@
-package ex2;
-
-public class ex2 {
-	public static void main(String[] args) {
-		InterfaceFuncional texto = () -> System.out.println("Teste Interface Funcional");
-		texto.show();
-	}
-
-}
